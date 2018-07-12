11a2f0e91949854c47b34c16fb8705cf88f1f2d0
Alpha Shuro <alphashuro@gmail.com>
2018-07-12 18:02:15 +0200

    add search button

--- src/App/Search/container/container.js
+++ src/App/Search/container/container.js
@@ -1,13 +1,15 @@
 import { connect } from "react-redux";
 import Search from "../search";
 import { changeText } from "./actions";
+import { fetchArtists } from "../../AutoComplete/container/actions";
 
 const mapStateToProps = state => ({
   text: state.deezer.text
 });
 
 const mapDispatchToProps = dispatch => ({
-  onChange: event => dispatch(changeText(event.target.value))
+  onTextChange: event => dispatch(changeText(event.target.value)),
+  onSearchClick: text => dispatch(fetchArtists(text))
 });
 
 export default connect(
--- src/App/Search/search.js
+++ src/App/Search/search.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-export default ({ text = "", onChange }) => (
+export default ({ text = "", onTextChange, onSearchClick }) => (
   <div>
-    <input type="text" value={text} onChange={onChange} />
+    <input type="text" value={text} onChange={onTextChange} />
+    <button onClick={() => onSearchClick(text)}>Search</button>
   </div>
 );
