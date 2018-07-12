f076524b21f42d6755bcc1d4ea5b11dae8348a49
Alpha Shuro <alphashuro@gmail.com>
2018-07-12 17:05:24 +0200

    display albums

--- src/DeezerSearch/Albums/Albums.js
+++ src/DeezerSearch/Albums/Albums.js
@@ -1,3 +1,13 @@
 import React from "react";
 
-export default () => <div>albums</div>;
+export default ({ artist, loading, albums, onClickAlbum }) =>
+  artist && (
+    <div>
+      <p>{artist.name}'s albums</p>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>{albums.map(album => <li key={album.id}>{album.title}</li>)}</ul>
+      )}
+    </div>
+  );
--- src/DeezerSearch/Albums/container/reducer.js
+++ src/DeezerSearch/Albums/container/reducer.js
@@ -1,3 +1,22 @@
+import { types } from "./actions";
+import { merge } from "ramda";
+
 export default (state, action) => {
-  return state;
+  const mergeState = merge(state);
+
+  switch (action.type) {
+    case types.FETCH_ALBUMS_REQUEST:
+      return mergeState({ loading: true });
+    case types.FETCH_ALBUMS_SUCCESS:
+      return mergeState({
+        loading: false,
+        data: action.albums
+      });
+    case types.FETCH_ALBUMS_FAILURE:
+      return mergeState({ loading: false, error: action.message });
+    case types.SELECT_ALBUM:
+      return mergeState({ selected: action.album });
+    default:
+      return state;
+  }
 };
