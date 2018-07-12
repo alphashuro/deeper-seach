142921c326c348c13e1f745318cd6a0abd40c711
Alpha Shuro <alphashuro@gmail.com>
2018-07-12 18:26:50 +0200

    reorder imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { Provider } from "react-redux";
+import "./index.css";
 import store from "./store";
+import App from "./App/App";
 
 ReactDOM.render(
   <Provider store={store}>
